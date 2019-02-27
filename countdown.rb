14c461530f71c0c85881c1b721621d7f7b7810f5
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-27 16:12:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 #number = 10
 def countdown(number)
   while number > 0
-"#{number} SECOND(S)!"
+puts "#{number} SECOND(S)!"
 number -= 1
  end  
  "HAPPY NEW YEAR!"
@@ -11,7 +11,7 @@ end
 
 def countdown_with_sleep(number)
    while number > 0
-"#{number} SECOND(S)!"
+puts "#{number} SECOND(S)!"
   sleep(1)
 number -= 1
  end 
